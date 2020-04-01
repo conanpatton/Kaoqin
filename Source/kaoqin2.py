61785d7f4066aabecc807265bd511b5e96696f1d
conanpatton <conanpatton@hotmail.com>
2020-04-01 14:40:27 +0800

    update

--- Source/kaoqin2.py
+++ Source/kaoqin2.py
@@ -1,10 +1,12 @@
 import sys
 import urllib.parse
 import urllib.request
-
+# import time
+from datetime import datetime, timezone, timedelta
 from enum import Enum
 from PySide2.QtWidgets import (QApplication, QDialog, QLineEdit, QPushButton,
                                QVBoxLayout, QWidget, QLabel, QTextEdit, QLineEdit)
+import pytz
 
 
 class Form(QWidget):
@@ -15,7 +17,7 @@ class Form(QWidget):
     def __init__(self):
         QWidget.__init__(self)
         # Create widgets
-        self.setFixedSize(250, 150)  # 设置窗口固定大小
+        self.setFixedSize(250, 160)  # 设置窗口固定大小
         self.setWindowTitle("考勤小程序")
 
         self.userNoLabel = QLabel("工号: ")
@@ -45,51 +47,64 @@ class Form(QWidget):
             lambda: self.Postdata(self.PushType.TAISYA))
 
     def Postdata(self, post_type):
-        request_url = 'http://10.110.168.126/cws/srwtimerec'
+        try:
+            request_url = 'http://10.110.168.126/cws/srwtimerec'
 
-        request_headers = {
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'Accept-Encoding': 'gzip, deflate',
-            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-            'Cache-Control': 'max-age=0',
-            'Content-Length': 56,
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Cookie': 'cwsstyle=font_s; JSESSIONID=0000O4-_09IaB5kZCncOT51_ZUS:-1',
-            'Host': '10.110.168.126',
-            'Origin': 'http://10.110.168.126',
-            'Proxy-Connection': 'keep-alive',
-            'Referer': 'http://10.110.168.126/cws/srwtimerec',
-            'Upgrade-Insecure-Requests': 1,
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36 Edg/79.0.309.71'
-        }
+            request_headers = {
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'Accept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+                'Cache-Control': 'max-age=0',
+                'Content-Length': 56,
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Cookie': 'cwsstyle=font_s; JSESSIONID=0000O4-_09IaB5kZCncOT51_ZUS:-1',
+                'Host': '10.110.168.126',
+                'Origin': 'http://10.110.168.126',
+                'Proxy-Connection': 'keep-alive',
+                'Referer': 'http://10.110.168.126/cws/srwtimerec',
+                'Upgrade-Insecure-Requests': 1,
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36 Edg/79.0.309.71'
+            }
 
-        syussya_request_datas = {
-            'dakoku': 'syussya',
-            # 'user_id': '0CB118023',
-            'user_id': self.userNo.text,
-            # 'password': 'Kyqiu123',
-            'password': self.pd.text,
-            'watch': ''
-        }
+            syussya_request_datas = {
+                'dakoku': 'syussya',
+                # 'user_id': '0CB118023',
+                'user_id': self.userNo.text(),
+                # 'password': 'Kyqiu123',
+                'password': self.pd.text(),
+                'watch': ''
+            }
 
-        taisya_request_datas = {
-            'dakoku': 'taisya',
-            # 'user_id': '0CB118023',
-            'user_id': self.userNo.text,
-            # 'password': 'Kyqiu123',
-            'password': self.pd.text,
-            'watch': ''
-        }
+            taisya_request_datas = {
+                'dakoku': 'taisya',
+                # 'user_id': '0CB118023',
+                'user_id': self.userNo.text(),
+                # 'password': 'Kyqiu123',
+                'password': self.pd.text(),
+                'watch': ''
+            }
 
-        if post_type == self.PushType.SYUSYA:
-            request_datas = urllib.parse.urlencode(syussya_request_datas)
-        elif post_type == self.PushType.TAISYA:
-            request_datas = urllib.parse.urlencode(taisya_request_datas)
+            if post_type == self.PushType.SYUSYA:
+                request_datas = urllib.parse.urlencode(syussya_request_datas)
+            elif post_type == self.PushType.TAISYA:
+                request_datas = urllib.parse.urlencode(taisya_request_datas)
 
-        request_datas = request_datas.encode('ascii')
-        res = urllib.request.Request(
-            url=request_url, headers=request_headers, data=request_datas)
-        response = urllib.request.urlopen(res)
+            request_datas = request_datas.encode('ascii')
+            res = urllib.request.Request(
+                url=request_url, headers=request_headers, data=request_datas)
+            response = urllib.request.urlopen(res)
+            GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
+            gmt_str = response.headers['Date']
+            gmt_dt = datetime.strptime(response.headers['Date'], GMT_FORMAT)
+            gmt_dt = gmt_dt.replace(tzinfo=pytz.utc)
+
+            beijing_dt = gmt_dt.astimezone()
+            BEIJING_FROMAT = '%a, %Y-%m-%d %H:%M:%S'
+            beijing_str = beijing_dt.strftime(BEIJING_FROMAT)
+            # print(beijing_str)
+            # print(datetime(response.headers['Date']).astimezone())
+        except urllib.error.URLError as err:
+            print(err)
 
 
 if __name__ == '__main__':
